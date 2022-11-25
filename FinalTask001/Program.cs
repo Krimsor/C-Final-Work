0fa1ab54d4299c0783884b23b06946bd675f0ea8
Vadim <vsuffering@gmail.com>
2022-11-25 14:49:37 +1000

    add 2 array and display array1

--- FinalTask001/Program.cs
+++ FinalTask001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу , которая из имеющегося массива строк формирует массив из строк, длина которых, меньше либо равна 3 символам.
+
+string[] array1 = new string[5] { "Russia", "-2", "hello", ":-)", "computer science" };
+
+string[] array2 = new string[array1.Length];
+
+Console.Write("[{0}]", string.Join(", ", array1));
