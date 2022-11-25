d01f56c8d304acb7ef042c1a5e073a836560600c
Vadim <vsuffering@gmail.com>
2022-11-25 14:50:31 +1000

    add void SecondArray method

--- FinalTask001/Program.cs
+++ FinalTask001/Program.cs
@@ -5,3 +5,16 @@ string[] array1 = new string[5] { "Russia", "-2", "hello", ":-)", "computer scie
 string[] array2 = new string[array1.Length];
 
 Console.Write("[{0}]", string.Join(", ", array1));
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
